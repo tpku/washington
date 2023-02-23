5cda72a53e09ba777d5935065bde006dfc5cad66
tpku <tpku@icloud.com>
2023-02-23 10:57:50 +0100

    Login & register blade content

---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,16 @@
+<form method="post" action="/user/register">
+    @csrf
+    <div>
+        <label for="name">Name</label>
+        <input name="name" id="name" type="name" />
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" />
+    </div>
+    <button type="submit">Register</button>
+</form>
